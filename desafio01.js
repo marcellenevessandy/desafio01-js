0a67edbde3828070c0ecf2abd3437e7fa4a6d663
Marcelle Neves Sandy <146496555+marcellenevessandy@users.noreply.github.com>
2023-11-14 19:42:29 -0300

    Adicionei o arquivo com o desafio.

---
+++ desafio01.js
@@ -0,0 +1,10 @@
+const empresa = {
+    nome: "Sx",
+    segmento: "Bancário",
+    endereco: {
+    rua: "Pedro simões",
+    numero: 22
+    }
+}
+
+console.log(`A empresa ${empresa.nome}, é do segmento ${empresa.segmento} e está localizada na rua ${empresa.endereco.rua}, número ${empresa.endereco.numero}.`)
